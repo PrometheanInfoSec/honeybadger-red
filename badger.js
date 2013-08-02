2bbbaa33c4aa2aa37a7774dc04cb16e7d4f921d7
LaNMaSteR53 <tjt1980@gmail.com>
2013-08-01 21:07:39 -0700

    updated the map control positions in badger.js to reflect api changes.

--- badger.js
+++ badger.js
@@ -36,53 +36,66 @@ function loadMap(json) {
         return;
     }
     var coords = new google.maps.LatLng(0,0);
-	var mapOptions = {
-	    zoom: 5,
-	    center: coords,
-	    mapTypeId: google.maps.MapTypeId.ROADMAP,
-	    mapTypeControl: true,
-	    navigationControlOptions: {
-		    style: google.maps.NavigationControlStyle.DEFAULT,
-		    position: google.maps.ControlPosition.RIGHT_BOTTOM
-		}
-	};
-	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-	var bounds = new google.maps.LatLngBounds();
+    var mapOptions = {
+        zoom: 5,
+        center: coords,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+            position: google.maps.ControlPosition.RIGHT_TOP
+        },
+        panControl: true,
+        panControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_BOTTOM
+        },
+        streetViewControl: true,
+        streetViewControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_BOTTOM
+        },
+        zoomControl: true,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_BOTTOM
+        },
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    var bounds = new google.maps.LatLngBounds();
 
-	function add_marker(opts, place) {
-	    var marker = new google.maps.Marker(opts);
-	    var infowindow = new google.maps.InfoWindow({
-	    	autoScroll: false,
-	        content: place.details
-	    });
-		google.maps.event.addListener(marker, 'click', function() {
-		    infowindow.open(map,marker);
-	    });
-		bounds.extend(opts.position);
-		return marker;
-	}
+    function add_marker(opts, place) {
+        var marker = new google.maps.Marker(opts);
+        var infowindow = new google.maps.InfoWindow({
+            autoScroll: false,
+            content: place.details
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+        });
+        bounds.extend(opts.position);
+        return marker;
+    }
 
-	for (key in json) {
+    for (key in json) {
         var marker = json[key];
         // add marker to map
         var currMarker = add_marker({
-        	position: new google.maps.LatLng(marker.lat,marker.lng),
-        		title:marker.ip+":"+marker.port,
-        		map:map},{
-        	details:'<div align="left" class="iwcontent">Target: '+marker.target
-	        	+ '<br />Agent: '+marker.agent+' @ '+marker.ip+':'+marker.port
-	        	+ '<br />Time: '+marker.time
-	        	+ '<br />User-Agent: '+marker.useragent
-	        	+ '<br />Latitude: '+marker.lat
-	        	+ '<br />Longitude: '+marker.lng
-	        	+ '<br />Accuracy: '+marker.acc
-	        	+ '<br />Comment: '+marker.comment
-	        	+ '<br /></div>'});
-		// set markers for latest hit from selected target
-		var hitTime = new Date(marker.time);
+            position: new google.maps.LatLng(marker.lat,marker.lng),
+                title:marker.ip+":"+marker.port,
+                map:map},{
+            details:'<div align="left" class="iwcontent">Target: '+marker.target
+                + '<br />Agent: '+marker.agent+' @ '+marker.ip+':'+marker.port
+                + '<br />Time: '+marker.time
+                + '<br />User-Agent: '+marker.useragent
+                + '<br />Latitude: '+marker.lat
+                + '<br />Longitude: '+marker.lng
+                + '<br />Accuracy: '+marker.acc
+                + '<br />Comment: '+marker.comment
+                + '<br /></div>'});
+        // set markers for latest hit from selected target
+        var hitTime = new Date(marker.time);
         if (typeof lastTime === 'undefined' || hitTime > lastTime) {
             var lastTime = hitTime;
-        	lastMarkerObj = currMarker;
+            lastMarkerObj = currMarker;
         }
         // set markers for the most accurate hit from selected target
         if (marker.acc != "Unknown") {
@@ -93,7 +106,7 @@ function loadMap(json) {
             }
         }
     }
-	map.fitBounds(bounds);
+    map.fitBounds(bounds);
 }
 
 function loadSummary(json) {
@@ -101,14 +114,14 @@ function loadSummary(json) {
         document.getElementById("summary").innerHTML = ""
         return;
     }
-	var accMarker = null;
-	for (key in json) {
+    var accMarker = null;
+    for (key in json) {
         var marker = json[key];
-		// set markers for latest hit from selected target
-		var hitTime = new Date(marker.time);
-		if (typeof lastTime === 'undefined' || hitTime > lastTime) {
+        // set markers for latest hit from selected target
+        var hitTime = new Date(marker.time);
+        if (typeof lastTime === 'undefined' || hitTime > lastTime) {
             var lastTime = hitTime;
-        	var lastMarker = marker;
+            var lastMarker = marker;
         }
         // set markers for the most accurate hit from selected target
         if (marker.acc != "Unknown") {
@@ -119,24 +132,24 @@ function loadSummary(json) {
             }
         }
     }
-	// last hit
-	var lastTime = parseDate(new Date(lastMarker.time));
-	htmlLast = "<u>Most Recent:</u> (<a href='javascript:openMarker(lastMarkerObj)'>show</a>)"
-		+ "<br />Agent: " + lastMarker.agent
-		+ "<br />Date: " + lastTime['month'] + "/" + lastTime['day'] + "/" + lastTime['year']
-		+ "<br />Time: " + lastTime['hours'] + ":" + lastTime['minutes'] + ":" + lastTime['seconds']
-		+ "<br /><br />";
-	// most accurate hit
-	if (accMarker != null) {
-    	htmlAcc = "<u>Most Accurate:</u> (<a href='javascript:openMarker(accMarkerObj)'>show</a>)"
-    		+ "<br />Agent: " + accMarker.agent
-    		+ "<br />Accuracy: " + accMarker.acc + "m";
-	} else {
-		htmlAcc = '';
-	}
-	// add to panel
-	var summary = document.getElementById("summary");
-	summary.innerHTML = htmlLast + htmlAcc;
+    // last hit
+    var lastTime = parseDate(new Date(lastMarker.time));
+    htmlLast = "<u>Most Recent:</u> (<a href='javascript:openMarker(lastMarkerObj)'>show</a>)"
+        + "<br />Agent: " + lastMarker.agent
+        + "<br />Date: " + lastTime['month'] + "/" + lastTime['day'] + "/" + lastTime['year']
+        + "<br />Time: " + lastTime['hours'] + ":" + lastTime['minutes'] + ":" + lastTime['seconds']
+        + "<br /><br />";
+    // most accurate hit
+    if (accMarker != null) {
+        htmlAcc = "<u>Most Accurate:</u> (<a href='javascript:openMarker(accMarkerObj)'>show</a>)"
+            + "<br />Agent: " + accMarker.agent
+            + "<br />Accuracy: " + accMarker.acc + "m";
+    } else {
+        htmlAcc = '';
+    }
+    // add to panel
+    var summary = document.getElementById("summary");
+    summary.innerHTML = htmlLast + htmlAcc;
 }
 
 function isEmpty(o) {
@@ -149,12 +162,12 @@ function isEmpty(o) {
 }
 
 function parseDate(dateTime) {
-	var dateArray = new Array();
-	dateArray['year'] = dateTime.getUTCFullYear();
-	dateArray['month'] = dateTime.getUTCMonth() + 1;
-	dateArray['day'] = dateTime.getUTCDate();
-	var hours = dateTime.getHours();
-	var minutes = dateTime.getMinutes();
+    var dateArray = new Array();
+    dateArray['year'] = dateTime.getUTCFullYear();
+    dateArray['month'] = dateTime.getUTCMonth() + 1;
+    dateArray['day'] = dateTime.getUTCDate();
+    var hours = dateTime.getHours();
+    var minutes = dateTime.getMinutes();
     var seconds = dateTime.getSeconds();
     if (hours < 10){ hours = "0" + hours; }
     if (minutes < 10){ minutes = "0" + minutes; }
@@ -190,47 +203,47 @@ function loadFilter(json) {
 }
 
 function loadTargets(init) {
-	$.get(servicePath, 'targets=_', function(data){
+    $.get(servicePath, 'targets=_', function(data){
         var json = $.parseJSON(data);
         var select = document.getElementById("target");
         if (select.selectedIndex > -1) {
-        	var selected = select.options[select.selectedIndex].value;
+            var selected = select.options[select.selectedIndex].value;
         } else {
-        	var selected = '';
+            var selected = '';
             init = true;
         }
         //select.setAttribute('size', 1);
         select.options.length = 0;
         for (var i=0; i<json.targets.length; i++) {
-        	var target = json.targets[i];
-        	var option = document.createElement("option");
-        	option.text = target;
-        	option.value = target;
-        	if (target == selected) {
-        		option.selected = true;
-        	}
-        	select.appendChild(option);
+            var target = json.targets[i];
+            var option = document.createElement("option");
+            option.text = target;
+            option.value = target;
+            if (target == selected) {
+                option.selected = true;
+            }
+            select.appendChild(option);
         }
         if (init) {
-        	select.selectedIndex = -1;
+            select.selectedIndex = -1;
         }
-	});
-	window.setTimeout(function() { loadTargets(false); },10000);
+    });
+    window.setTimeout(function() { loadTargets(false); },10000);
 }
 
 function loadClock() {
     $.get(servicePath, 'time=_', function(data){
-    	var json = $.parseJSON(data);
-    	var serverTime = new Date(json.dtg);
-    	var localTime = new Date();
-    	var diffTime = serverTime - localTime;
+        var json = $.parseJSON(data);
+        var serverTime = new Date(json.dtg);
+        var localTime = new Date();
+        var diffTime = serverTime - localTime;
         tick(diffTime);
     });
 }
 
 function tick(diffTime){
-	var currTime = new Date();
-	currTime.setTime(currTime.getTime() + diffTime);
+    var currTime = new Date();
+    currTime.setTime(currTime.getTime() + diffTime);
     var clock_hours = currTime.getHours();
     var clock_minutes = currTime.getMinutes();
     var clock_seconds = currTime.getSeconds();
@@ -243,17 +256,17 @@ function tick(diffTime){
 }
 
 function purge(type) {
-	if (confirm("Are you sure you want to purge the "+type+"?")) {
+    if (confirm("Are you sure you want to purge the "+type+"?")) {
         $.get(servicePath, 'purge='+type, function(data){
-        	var json = $.parseJSON(data);
+            var json = $.parseJSON(data);
             var note = document.getElementById("notification");
             note.innerHTML = json.msg
             note.style.visibility = "visible";
             setTimeout(function() { note.style.visibility = "hidden"; }, 3000);
         });
-	}
+    }
 }
 
 function openMarker(marker) {
-	google.maps.event.trigger(marker, "click");
+    google.maps.event.trigger(marker, "click");
 }
