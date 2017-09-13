f82699dbc2a8bf2b770cfc8b131f9deae62f2caf
zaeyx <ben@prometheaninfosec.com>
2017-09-13 14:33:07 +0000

    demo.html -> php

--- demo.php
+++ demo.php
@@ -1,4 +1,18 @@
 <!doctype html>
+<?php
+
+function url(){
+  return explode("/demo.php", sprintf(
+    "%s://%s%s",
+    isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+    $_SERVER['SERVER_NAME'],
+    $_SERVER['REQUEST_URI']
+  ))[0];
+}
+
+
+?>
+
 <head>
 <script type="text/javascript" src="honey.js"></script>
 <style>
@@ -8,7 +22,7 @@
 </head>
 <body>
 <h1>This is the Demo page for the crazy, nastyass "Honey Badger".</h1>
-<img src="service.php?target=Demo_Page&agent=HTML" onerror="go('service.php','Demo_Page','honey.jar',true,true,5000);" width="1px" height="1px" />
+<img src="<?php echo url(); ?>/service.php?target=Demo_Page&agent=HTML" onerror="go('<?php echo url(); ?>/service.php','Demo_Page','honey.jar',true,true,5000);" width="1px" height="1px" />
 
 <br />
 <div class="g" >
